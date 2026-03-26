267d49c5ed3001bffa0c4d7a99ef505231fcdc63
lucym2008 <leonardomachadoarroyo14@gmail.com>
2026-03-25 21:03:37 -0300

    first commit

---
+++ BibliotecaDigital/src/main/java/com/leoautonomo/BibliotecaDigital/BibliotecaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.leoautonomo.BibliotecaDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDigitalApplication.class, args);
+	}
+
+}
---
+++ BibliotecaDigital/src/main/java/com/leoautonomo/BibliotecaDigital/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.leoautonomo.BibliotecaDigital;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BibliotecaDigitalApplication.class);
+	}
+
+}
